94439a18b48ea706734348f5a1678a80a620338f
tomrcc <tom@cloudcannon.com>
2024-01-02 18:08:15 +1300

    Fix portfolio pages

--- pages/portfolio/[slug].vue
+++ pages/portfolio/[slug].vue
@@ -4,8 +4,7 @@
     <div class="w-full">
       <!-- Hero -->
       <div
-        class="h-[250px] lg:h-[500px] w-full overflow-hidden bg-cover relative flex justify-center items-center"
-      >
+        class="h-[250px] lg:h-[500px] w-full overflow-hidden bg-cover relative flex justify-center items-center">
         <nuxt-img
           :src="pageData.hero_image"
           alt=""
@@ -18,11 +17,9 @@
               '% ' +
               pageData.hero_image_filters.position.vertical +
               '%',
-          }"
-        />
+          }" />
         <h1
-          class="text-center font-bold text-[2.5rem] lg:text-[6rem] my-12 relative z-10 text-white"
-        >
+          class="text-center font-bold text-[2.5rem] lg:text-[6rem] my-12 relative z-10 text-white">
           {{ pageData.title.toUpperCase() }}
         </h1>
       </div>
@@ -34,28 +31,24 @@
         </div>
         <div
           class="grid grid-cols-3 pr-2 pb-12"
-          :style="{ gap: pageData.horizontal_gap + 'px' }"
-        >
+          :style="{ gap: pageData.horizontal_gap + 'px' }">
           <div
             class="flex flex-col"
-            :style="{ gap: pageData.vertical_gap + 'px' }"
-          >
+            :style="{ gap: pageData.vertical_gap + 'px' }">
             <div v-for="image in pageData.images.column_one">
               <nuxt-img loading="lazy" :src="image" alt="" />
             </div>
           </div>
           <div
             class="flex flex-col"
-            :style="{ gap: pageData.vertical_gap + 'px' }"
-          >
+            :style="{ gap: pageData.vertical_gap + 'px' }">
             <div v-for="image in pageData.images.column_two">
               <nuxt-img loading="lazy" :src="image" alt="" />
             </div>
           </div>
           <div
             class="flex flex-col"
-            :style="{ gap: pageData.vertical_gap + 'px' }"
-          >
+            :style="{ gap: pageData.vertical_gap + 'px' }">
             <div v-for="image in pageData.images.column_three">
               <nuxt-img loading="lazy" :src="image" alt="" />
             </div>
@@ -67,9 +60,28 @@
 </template>
 
 <script setup>
-const { portfolioMenu, pageData, route } = defineProps({
-  portfolioMenu: Array,
-  pageData: Object,
-  route: Object,
+const { page } = useContent();
+const pageData = page.value;
+const route = useRoute();
+const head = generateHead(pageData, route);
+useHead(head);
+
+const allPortfolios = await queryContent('portfolio').find();
+
+const portfolioMenu = allPortfolios.map((file) => {
+  const filePath = file._path.split('.')[0];
+  if (filePath === '/portfolio/index') {
+    return {
+      path: '/portfolio/',
+      title: 'General',
+      active: filePath === pageData._path ? true : false,
+    };
+  } else {
+    return {
+      path: `${filePath}/`,
+      title: file.title,
+      active: filePath === pageData._path ? true : false,
+    };
+  }
 });
 </script>
